18fcbc4267b3a1ca3c04968bd7fe9bd81c03effa
TheSunCat <thesuncat123@gmail.com>
2022-08-04 23:58:48 +0200

    Prepare for publishing

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <system_theme/system_theme_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  SystemThemePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SystemThemePlugin"));
+}
